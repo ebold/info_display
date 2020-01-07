6a090b95e5b0f1e3c54dfac7bc8a44179ebe201c
Enkhbold Ochirsuren <boldoodd@gmail.com>
2020-01-07 22:58:31 +0100

    python: workaround to an intermittent UART connection via Stlink VCP
    Regarding VCP issue refer to [1] and [2]:
    [1] https://community.st.com/s/question/0D50X00009XkhUU/stlink-vcp-with-flow-control
    [2] https://electronics.stackexchange.com/questions/288187/is-it-possible-to-get-st-links-virtual-com-port-working-without-launching-any-p

--- python/serialtest.py
+++ python/serialtest.py
@@ -1,7 +1,11 @@
 #!/bin/env python
 
-# Send/print the current system time in given format
-# when next minute fires
+# Send/print the current system time in given format when next minute fires.
+
+# The UART transmission of datetime string to Nucleo is done with ms-delay
+# between each byte in the string to overcome VCP issue in Stlink. [1][2]
+# [1] https://community.st.com/s/question/0D50X00009XkhUU/stlink-vcp-with-flow-control
+# [2] https://electronics.stackexchange.com/questions/288187/is-it-possible-to-get-st-links-virtual-com-port-working-without-launching-any-p
 
 import time
 import serial
@@ -10,6 +14,7 @@ import datetime
 DEVICE = '/dev/ttyACM0'  # serial device name
 BAUDRATE = 115200        # default baudrate
 DELAY = 3                # default polling interval
+DELAY_MS = 0.001         # 1ms delay
 FORMAT = '%H:%M'         # time format in use
 VERBOSE = False          # verbose flag (set to True to get output message)
 DEBUG = False            # debug flag (set to True when serial device is not available)
@@ -24,13 +29,23 @@ def seconds_left_to_minute():
         print seconds_left, "seconds left until", next_minute.time().strftime(FORMAT)
     return seconds_left
 
+# send a string to serial device with delay
+def send_with_delay(string):
+    if not DEBUG and ser == None:
+        return
+    for s in string:
+        if DEBUG:
+            print s
+        else:
+            ser.write(s)
+        time.sleep(DELAY_MS) # some ms delay
+
 with serial.Serial(DEVICE, BAUDRATE) as ser:
     # time at start
     now = datetime.datetime.now().time()
     last = now.strftime(FORMAT)
     print "Now:", now, "or", last
-    if not DEBUG:
-        ser.write(last)
+    send_with_delay(last)
     wait = seconds_left_to_minute()
     if not VERBOSE:
         print wait, "seconds left until next full minute"
@@ -45,7 +60,6 @@ with serial.Serial(DEVICE, BAUDRATE) as ser:
             last = now.strftime(FORMAT)
             if VERBOSE:
                 print "Now:", now, "or", last
-            if not DEBUG:
-                ser.write(last)  # send time to serial device
+            send_with_delay(last)  # send time to serial device
             wait = seconds_left_to_minute()
         time.sleep(wait)
