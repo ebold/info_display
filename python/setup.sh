2cbadc5ef99fe1f78e484061b54986d7442cb58e
Enkhbold Ochirsuren <boldoodd@gmail.com>
2020-01-12 15:38:04 +0100

    shell: added a setup script for the UART communication with Nucleo

---
+++ python/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Set up the serial communication between the BeagleBone Black board (running
+# ArchLinuxARM) and the STM32 Nucleo board (with STLink embedded) via USB.
+# The Nucleo board is powered only by USB.
+
+cur_user=$(whoami)
+
+if [ $cur_user != 'root' ]; then
+  echo "Run it under root administration!"
+  exit 1
+fi
+
+udev_rule_file='49-stlinkv2-1.rules'
+udev_rule_dir='/etc/udev/rules.d/'
+nucleo_service_file='nucleo.service'
+systemd_services_dir='/etc/systemd/system/'
+
+echo "Set up an environment needed to communicate with the Nucleo board via USB."
+
+if [ ! -e $udev_rule_file ]; then
+  echo "Error: cannot find $udev_rule_file"
+  exit 1
+fi
+
+if [ ! -e $nucleo_service_file ]; then
+  echo "Error: cannot find $nucleo_service_file"
+  exit 1
+fi
+
+cp $udev_rule_file $udev_rule_dir
+echo "Copied $udev_rule_file into $udev_rule_dir"
+
+cp $nucleo_service_file $systemd_services_dir
+echo "Copied $nucleo_service_file into $systemd_services_dir"
+
+echo "Ready to communication with the Nucleo board!"
+echo "Please reboot to apply system changes"
