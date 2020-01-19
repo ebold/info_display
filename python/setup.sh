c24f3a7d95b16143f34bb586ff5ad63e05e1388f
Enkhbold Ochirsuren <boldoodd@gmail.com>
2020-01-19 17:13:06 +0100

    setup: install user scripts for Nucleo into the '/usr/local/bin' directory

--- python/setup.sh
+++ python/setup.sh
@@ -11,10 +11,13 @@ if [ $cur_user != 'root' ]; then
   exit 1
 fi
 
-udev_rule_file='49-stlinkv2-1.rules'
-udev_rule_dir='/etc/udev/rules.d/'
-nucleo_service_file='nucleo.service'
-systemd_services_dir='/etc/systemd/system/'
+udev_rule_file='49-stlinkv2-1.rules'         # udev rule with signal to systemd on adding ttyACM0 device
+udev_rule_dir='/etc/udev/rules.d/'           # standard location of the udev rules
+nucleo_service_file='nucleo.service'         # user service (controlled by systemd)
+systemd_services_dir='/etc/systemd/system/'  # standard location of the user service file
+user_script_dir='/usr/local/bin/'            # standard location of the user scripts
+user_shell_script='serialcomm_nucleo.sh'     # user shell script that starts the serial communication to Nucleo
+user_python_script='sync_nucleo_datetime.py' # user python script that synchronize datetime in Nucleo
 
 echo "Set up an environment needed to communicate with the Nucleo board via USB."
 
@@ -33,6 +36,9 @@ echo "Copied $udev_rule_file into $udev_rule_dir"
 
 cp $nucleo_service_file $systemd_services_dir
 echo "Copied $nucleo_service_file into $systemd_services_dir"
+
+cp $user_shell_script $user_python_script $user_script_dir
+echo "Copied $user_shell_script $user_python_script into $user_script_dir"
 
 echo "Ready to communication with the Nucleo board!"
 echo "Please reboot to apply system changes"
